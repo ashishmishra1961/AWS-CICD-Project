7decbfe58f32170305d8e2af9a10c8428a309e8d
ashishmishra1961 <87991555+ashishmishra1961@users.noreply.github.com>
2024-05-20 19:42:57 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull premajanakumar/hotel-app:latest
+docker pull ashishmishra1961/hotel-app:latest
 
 # Run the Docker image as a container
-docker run -dit -p 80:80 premajanakumar/hotel-app
+docker run -dit -p 80:80 ashishmishra1961/hotel-app
